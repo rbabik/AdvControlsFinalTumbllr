17c6a71000e969d861d4f189418f7caf1862198b
Evan Cohen <evancohe400@gmail.com>
2021-12-10 19:05:42 -0500

    Final PID code

---
+++ Arduino PID/PID_Final/PID_Final.ino
@@ -0,0 +1,432 @@
+/* Include necessary header files */
+#include "PinChangeInt.h"
+#include "MsTimer2.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "KalmanFilter.h"
+
+/* TB6612FNG motor driver signal pins */
+#define IN1_L 7
+#define IN1_R 12
+#define PWM_L 5
+#define PWM_R 6
+#define STBY 8
+
+/* Encoder count signal pins */
+#define PinA_left 2
+#define PinA_right 4
+
+/* Sensor Reading Variables */
+MPU6050 mpu; //Instantiate a MPU6050 object with the object name MPU.
+int16_t ax, ay, az, gx, gy, gz; // Variables for IMU readings
+volatile long right_encoder = 0;
+volatile long left_encoder = 0;
+/* 
+ The volatile long type is used to ensure that the value is valid when the external 
+ interrupt pulse count value is used in other functions 
+ */
+float num_of_encoder_counts_per_rev = 780.0;
+float thousand_by_num_of_encoder_counts_per_rev = 1000.0/num_of_encoder_counts_per_rev;
+
+/* IMU Callibration Variables */ 
+
+long gyroXCalli = -415, gyroYCalli = 36, gyroZCalli = -292; // Obtained by callibrating gyroscope values
+long accelXCalli = 739, accelYCalli = -73, accelZCalli = 15748; // Obtained by callibrating accelerometer values
+
+/* Motor PWM Input Values */
+long motor_left = 0;
+long motor_right = 0;
+
+/* Time variables */
+unsigned long time;
+unsigned long prev_time_encoder = 0;
+unsigned long startTime_left = 0;
+unsigned long startTime_right = 0;
+int sampling_rate = 5; // in milliseconds
+
+/* Encoder to speed measurement variables */
+
+int encoder_count_max = 20;
+float motor_left_ang_vel = 0;
+float motor_right_ang_vel = 0;
+
+/* Robot parameters */
+
+float wheel_rad = 0.0335; // radius of the wheel in metres
+float lat_dist = 0.194; // distance between ends of the wheels in metres
+/********************Initialization settings********************/
+void setup() {
+  
+  /* TB6612FNGN Motor Driver module control signal initialization */
+  pinMode(IN1_L, OUTPUT); //Control the direction of motor 1, 1 is forward, 0 is reverse
+  pinMode(IN1_R, OUTPUT); //Control the direction of motor 1, 1 is forward, 0 is reverse
+  pinMode(PWM_L, OUTPUT); //PWM of left motor
+  pinMode(PWM_R, OUTPUT); //PWM of right motor
+  pinMode(STBY, OUTPUT); //enable TB6612FNG
+  
+  /* Initializing motor drive module */
+  digitalWrite(IN1_L, HIGH);
+  digitalWrite(IN1_R, LOW);
+  digitalWrite(STBY, HIGH);
+  analogWrite(PWM_L, 0);
+  analogWrite(PWM_R, 0);
+  
+  /* Initialize I2C bus */
+  Wire.begin(); //Add I2C bus sequence
+  Serial.begin(57600); //Open the serial port and set the baud rate
+  delay(1500);
+  mpu.initialize(); //Initialization MPU6050
+  delay(2);
+
+  /* 
+   Uncomment the next two lines only once and store the callibration values in the 
+   global variables defined for callibration
+  */
+//  Serial.println("Started Calibration");
+//  callibrateGyroValues();
+//  Serial.println("Finished Gyro Calli, starting Accel");
+//  callibrateAccelValues();
+  
+  time = millis();
+  startTime_left = millis();
+  startTime_right = millis();
+  
+  /* Interrupt function to count the encoder pulses */
+  attachInterrupt(digitalPinToInterrupt(PinA_left), encoder_left, CHANGE);
+  attachPinChangeInterrupt(PinA_right, encoder_right, CHANGE);
+
+  MsTimer2::set(sampling_rate, mainfunc);
+  MsTimer2::start();
+  
+  Serial.print("Setup Done!\n");
+}
+
+/***************************************************************************************/
+/*
+ This section contains functions that you can use to build your controllers
+*/
+/***************************************************************************************/
+
+/* 
+encoder_left() counts encoder pulses of the left wheel motor and stores it in a 
+global variable 'left_encoder'
+*/
+void encoder_left(){left_encoder++;}
+  
+/* 
+encoder_right() counts encoder pulses of the right wheel motor and stores it in a 
+global variable 'right_encoder'
+*/
+void encoder_right(){right_encoder++;}
+
+
+/* 
+SetLeftWheelSpeed() takes one input which will be set as the PWM input to the left motor.
+If the value is outside the range (-255,255), then the input will be saturated and the 
+motor PWM will be set. The value is also written to the global variable 'motor_left'
+*/
+void SetLeftWheelSpeed(double speed_val)
+{
+  // Saturate the input to the range (-255,255)
+  if (speed_val > 255) { speed_val = 255; }
+  else if (speed_val < -255) { speed_val = -255; }
+  
+  motor_left = speed_val;
+  
+  if (speed_val < 0)
+  {
+    digitalWrite(IN1_L, 1);
+    analogWrite(PWM_L, -speed_val);
+  }
+  else
+  {
+    digitalWrite(IN1_L, 0);
+    analogWrite(PWM_L, speed_val);
+  }
+}
+
+/* 
+SetRightWheelSpeed() takes one input which will be set as the PWM input to the right motor.
+If the value is outside the range (-255,255), then the input will be saturated and the 
+motor PWM will be set. The value is also written to the global variable 'motor_right'
+*/
+void SetRightWheelSpeed(double speed_val)
+{
+  // Saturate the input to the range (-255,255)
+  if (speed_val > 255) { speed_val = 255; }
+  else if (speed_val < -255) { speed_val = -255; }
+  
+  motor_right = speed_val;
+  
+  if (speed_val < 0)
+  {
+    digitalWrite(IN1_R, 1);
+    analogWrite(PWM_R, -speed_val);
+  }
+  else
+  {
+    digitalWrite(IN1_R, 0);
+    analogWrite(PWM_R, speed_val);
+  }
+}
+
+/*
+readIMU() creates an MPU6050 class object and calls the function to read the six axis IMU.
+The values are stored in the global variables ax,ay,az,gx,gy,gz where ax,ay,az are the 
+accelerometer readings and gx,gy,gz are the gyroscope readings. 
+*/
+void readIMU()
+{
+  MPU6050 mpu_obj;
+  mpu_obj.getMotion6(&ax,&ay,&az,&gx,&gy,&gz);
+}
+
+/*
+readEncoder() takes the encoder pulse counts and calculates the angular velocities of the
+wheels and stores it in the global variables 'motor_left_ang_vel' and 'motor_right_ang_vel'
+*/
+void readEncoder()
+{ 
+  // Encoder Calculations
+  // angular velocity = (encoder_reading/num_of_counts_per_rotation)*(2*pi/sampling_time)
+  
+  motor_left_ang_vel = (float) 2 * 3.1415 * left_encoder * (float)thousand_by_num_of_encoder_counts_per_rev / (float)(time - startTime_left);  
+  if (motor_left < 0){
+    motor_left_ang_vel = -motor_left_ang_vel;}
+  startTime_left = time;
+  left_encoder = 0;  
+  
+  motor_right_ang_vel = (float) 2 * 3.1415 * right_encoder * (float)thousand_by_num_of_encoder_counts_per_rev / (float)(time - startTime_right);  
+  if (motor_right < 0){
+    motor_right_ang_vel = -motor_right_ang_vel;}
+  startTime_right = time;
+  right_encoder = 0;  
+}
+
+/* 
+printIMU() prints the IMU readings to the serial monitor in the following format:
+ax,ay,az,gx,gy,gz
+*/
+void printIMU()
+{
+  Serial.print(ax);
+  Serial.print(',');
+  Serial.print(ay);
+  Serial.print(',');
+  Serial.print(az);
+  Serial.print(',');
+  Serial.print(gx);
+  Serial.print(',');
+  Serial.print(gy);
+  Serial.print(',');
+  Serial.print(gz);
+  Serial.print('\n');
+}
+
+/* 
+printEncoder() prints the encoder readings to the serial monitor in the following format:
+motor_left_ang_vel, motor_right_ang_vel
+*/
+void printEncoder()
+{
+  Serial.print(motor_left_ang_vel);
+  Serial.print(',');
+  Serial.print(motor_right_ang_vel);
+  Serial.print('\n');
+}
+
+/* 
+printAllData() prints the IMU readings, encoder readings and PWM inputs to the motor to
+the serial monitor in the following format:
+ax,ay,az,gx,gy,gz,motor_left_ang_vel,motor_right_ang_vel,motor_left,motor_right
+*/
+void printAllData()
+{
+  Serial.print(time);
+  Serial.print(',');
+  Serial.print(ax);
+  Serial.print(',');
+  Serial.print(ay);
+  Serial.print(',');
+  Serial.print(az);
+  Serial.print(',');
+  Serial.print(gx);
+  Serial.print(',');
+  Serial.print(gy);
+  Serial.print(',');
+  Serial.print(gz);
+  Serial.print(',');
+  Serial.print(motor_left_ang_vel);
+  Serial.print(',');
+  Serial.print(motor_right_ang_vel);
+  Serial.print(',');
+  Serial.print(motor_left);
+  Serial.print(',');
+  Serial.print(motor_right);
+  Serial.print('\n');
+}
+
+/*
+callibrateGyroValues() gets the gyroscope readings n times and calculates the average of
+the values to find the sensor bias. The callibration values are printed as:
+gyroXCalli,gyroYCalli,gyroZCalli
+*/
+double gx_callibration;
+void callibrateGyroValues() 
+{
+    int n = 10000;
+    for (int i=0; i < n; i++) 
+    {
+      readIMU();
+      gyroXCalli = gyroXCalli + gx;
+      gyroYCalli = gyroYCalli + gy;
+      gyroZCalli = gyroZCalli + gz;
+    }
+    gyroXCalli = gyroXCalli/n;
+    gyroYCalli = gyroYCalli/n;
+    gyroZCalli = gyroZCalli/n;
+    Serial.print(gyroXCalli);
+    Serial.print(',');
+    Serial.print(gyroYCalli);
+    Serial.print(',');
+    Serial.print(gyroZCalli);
+    Serial.print('\n');
+}
+
+/*
+callibrateAccelValues() gets the accelerometer readings n times and calculates the average of
+the values to find the sensor bias. The callibration values are printed as:
+accelXCalli,accelYCalli,accelZCalli
+*/
+void callibrateAccelValues() 
+{
+    int n = 10000;
+    for (int i=0; i < n; i++) 
+    {
+      readIMU();
+      accelXCalli = accelXCalli + ax;
+      accelYCalli = accelYCalli + ay;
+      accelZCalli = accelZCalli + az;
+    }
+    accelXCalli = accelXCalli/n;
+    accelYCalli = accelYCalli/n;
+    accelZCalli = accelZCalli/n;
+    Serial.print(accelXCalli);
+    Serial.print(',');
+    Serial.print(accelYCalli);
+    Serial.print(',');
+    Serial.print(accelZCalli);
+    Serial.print('\n');
+}
+
+/***************************************************************************************/
+/***************** Write your custom variables and functions below *********************/
+/***************************************************************************************/
+
+/* Added macros */
+float alpha = 0.97;
+float prevAngle;
+float currAngle;
+float Gyro_x;
+float curr_angular_rate;
+float prev_angular_rate;
+void startAngle(void)
+{
+  prevAngle = atan2(ay,az)*57.3;
+}
+
+/* Error and Ref variables */
+float e_p, e_d;
+float e_p_prev = 0;
+float e_d_prev = 0;
+float refAngle = -0.95; //Set back to account for COM with guard
+float refAngleRate = 0.;
+double balanceCmd;
+
+KalmanFilter kalmanfilter;
+double kp_balance =50, kd_balance = .7, ki_balance=2;//1.5;
+float dt = 0.005, Q_angle = 0.001, Q_gyro = 0.005, R_angle = 0.5, C_0 = 1, K1 = 0.05;
+float kalmanfilter_angle;
+float kalmanfilter_angle_prev = 0;
+float kalmanfilter_angle_dot;
+float motorCmd;
+
+//Sets our angle estimate to be Kalman Filter estimate from base code, sets the angle rate as numeric derivative of this
+void getAngle(void) 
+{
+  kalmanfilter.Angle(ax, ay, az, gx, gy, gz, dt, Q_angle, Q_gyro, R_angle, C_0, K1);
+  kalmanfilter_angle = kalmanfilter.angle;
+  kalmanfilter_angle_dot = (kalmanfilter_angle - kalmanfilter_angle_prev) / dt;
+  kalmanfilter_angle_prev = kalmanfilter_angle;
+}
+
+float smooth(float val) { //Our moving average filter to be used on the angle rate estimate
+
+  const int numReadings  = 10;
+  static float readings [numReadings];
+  static int readIndex  = 0;
+  static float total  = 0;
+  float average;
+  total = total - readings[readIndex];
+  readings[readIndex] = val;
+  total = total + readings[readIndex];
+  readIndex = readIndex + 1;
+  if (readIndex >= numReadings) {
+    readIndex = 0;
+  }
+  average = total / numReadings;
+  return average;
+}
+
+void balancePID(void)
+{
+
+  e_p = -.95 - kalmanfilter_angle; 
+  e_d = refAngleRate - smooth(kalmanfilter_angle_dot); //Angle is moving averaged filtered using smooth()
+  static float e_i = 0;
+  if(e_p < 15) {e_i += e_p;}
+  
+  balanceCmd = kp_balance * e_p + kd_balance * e_d + ki_balance * e_i;
+  balanceCmd = -balanceCmd;
+}
+
+// variable to account for motor offset
+double deadZoneOffset = 18.3;
+
+//Where our all relevant functions to run our control algorithm are called
+void mainfunc()
+{
+
+  sei();
+  time = millis(); 
+  if (time - prev_time_encoder >= encoder_count_max)
+  {
+    readEncoder();
+    prev_time_encoder = time;
+  }
+  
+  readIMU();
+  getAngle(); 
+  balancePID(); //Our controller called here
+  
+  //do nothing if within margin of reference
+  float margin = .1;
+  if(abs(kalmanfilter_angle) <= margin){ 
+    balanceCmd = 0;
+  }
+
+  motorCmd = balanceCmd; //Just using a angle control, no position 
+
+  //Controller probably blown up or being picked up if this happens: do nothing
+  if (abs(kalmanfilter_angle) > 22) {
+    motorCmd = 0;
+  }
+  
+  SetRightWheelSpeed(motorCmd);
+  SetLeftWheelSpeed(motorCmd);
+}
+
+void loop()
+{
+
+}
